4e9e86566fe2bcbf11eff434e87e47a9b93e6439
Sergii Ripa <sergey.ripa@jivygroup.com>
2018-04-12 11:45:36 +0300

    move streams to rx-2

--- streams/src/test/java/io/scalecube/streams/Rx3Tests.java
+++ streams/src/test/java/io/scalecube/streams/Rx3Tests.java
@@ -1,64 +1,47 @@
 package io.scalecube.streams;
 
-import static org.junit.Assert.assertEquals;
+import static io.reactivex.schedulers.Schedulers.computation;
 
-import io.reactivex.BackpressureStrategy;
-import io.reactivex.Flowable;
-import io.reactivex.FlowableSubscriber;
+import io.reactivex.processors.PublishProcessor;
 
 import org.junit.Test;
-import org.reactivestreams.Subscription;
 
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 public class Rx3Tests {
-  String result = "";
 
-  int N = 5;
-  int N_SUBSCRIBERS = 1;
+  int N = 10_000;
 
-  @Test
-  public void testSimple() throws InterruptedException {
-    result = "";
-    BackpressureStrategy buffer = BackpressureStrategy.ERROR;
-    Flowable<Integer> flowable = Flowable.create(flowableEmitter -> {
-      IntStream.rangeClosed(1, N).forEach(flowableEmitter::onNext);
-      flowableEmitter.onComplete();
-    }, buffer);
-    AtomicInteger rcvd = new AtomicInteger();
-    AtomicInteger subscriberId = new AtomicInteger();
+//  @Test
+//  public void testSimple() throws InterruptedException {
+//    AtomicInteger subscriber = new AtomicInteger();
+//    PublishProcessor<Integer> source = PublishProcessor.create();
+//    source
+//        .observeOn(computation())
+//        .buffer(1000)
+//        .subscribe(
+//            i -> heavyCompute(subscriber.getAndIncrement(), i),
+//            t -> t.printStackTrace());
+//    IntStream.rangeClosed(1, N).forEach(source::onNext);
+//    Thread.currentThread().join();
+//  }
+//
+//  static int heavyCompute(int subId, int i) {
+//    System.out.println("Subscriber:" + subId + " processing:" + i);
+//    return compute(1_000_000);
+//  }
+//
+//  static int lightCompute() {
+//    return compute(10);
+//  }
+//
+//  private static int compute(int i2) {
+//    int n = i2;
+//    for (int i = 0; i < n; i++) {
+//      Math.hypot(1.0, 1.0);
+//    }
+//    return n;
+//  }
 
-    CountDownLatch latch = new CountDownLatch(N);
-    for (int i = 0; i < N_SUBSCRIBERS; i++)
-      flowable.subscribeWith(new FlowableSubscriber<Integer>() {
-        int id = subscriberId.incrementAndGet();
-
-        @Override
-        public void onSubscribe(Subscription s) {
-          System.out.println("Subscriber " + id + ": Initialization passed");
-          s.request(Long.MAX_VALUE);
-        }
-
-        @Override
-        public void onNext(Integer integer) {
-          int k = rcvd.incrementAndGet();
-          System.out.println("Subscriber " + id + ":" + k);
-        }
-
-        @Override
-        public void onError(Throwable throwable) {
-          latch.countDown();
-        }
-
-        @Override
-        public void onComplete() {
-          System.out.println("Subscriber " + id + ":End");
-          latch.countDown();
-        }
-      });
-    latch.await();
-    assertEquals(N * N_SUBSCRIBERS, rcvd.get());
-  }
 }
